5ff27116563d53b974232cd99da6dccf6c4c82b2
Rahmaaa23 <nuzuliarahmatunnisa@gmail.com>
2025-07-18 00:23:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -71,10 +71,7 @@ Di sini kamu dapat:
         st.markdown("""
 **Massa molar** adalah jumlah massa dari semua atom dalam satu mol senyawa, dinyatakan dalam g/mol.
 """)
-        with st.expander("📘 Bagaimana cara mencari massa molar?"):    
-    st.markdown("""
-**Massa molar** senyawa dihitung dengan menjumlahkan massa atom relatif (Ar) tiap unsur dikalikan jumlah atomnya. Misalnya pada H2O: hidrogen (Ar = 1) ada 2 atom, sehingga totalnya 2 x 1 = 2; oksigen (Ar = 16) ada 1 atom, jadi 1 x 16 = 16. Maka, massa molar H2O adalah 2 + 16 = **18 g/mol**.
-""")
+       
    
 
 
