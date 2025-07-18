cf84c662fbcde18bd376e405284be82182bf3fd5
Rahmaaa23 <nuzuliarahmatunnisa@gmail.com>
2025-07-18 08:29:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -178,16 +178,16 @@ Contoh:
         """)
 
 rumus = st.text_input("Masukkan Rumus Kimia", placeholder="Contoh: H2O, NaCl, C6H12O6")
-        def hitung_massa_molar(rumus):
-            pattern = r'([A-Z][a-z]?)(\d*)'
-            elemen = re.findall(pattern, rumus)
-            massa_total = 0
-            for simbol, jumlah in elemen:
-                if simbol not in massa_atom:
-                    return None, f"Unsur '{simbol}' tidak ditemukan."
-                    n = int(jumlah) if jumlah else 1
-                    massa_total += massa_atom[simbol] * n
-                    return massa_total, None
+def hitung_massa_molar(rumus):
+    pattern = r'([A-Z][a-z]?)(\d*)'
+    elemen = re.findall(pattern, rumus)
+    massa_total = 0
+    for simbol, jumlah in elemen:
+        if simbol not in massa_atom:
+            return None, f"Unsur '{simbol}' tidak ditemukan."
+            n = int(jumlah) if jumlah else 1
+            massa_total += massa_atom[simbol] * n
+            return massa_total, None
 
     if st.button("Hitung Massa Molar"):
         if rumus.strip() == "":
